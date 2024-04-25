f7533a9e1efc1abe5e83629fee52de41edf24c09
dalienst <oduordalienst@gmail.com>
2024-04-26 02:04:36 +0300

    work on deliveries

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { appLinks } from "../constants/links";
 
 function Delivery() {
   return (
-    <div>Delivery</div>
-  )
+    <>
+      <div className="container py-3">
+        <h5>
+          <span>
+            <Link to={appLinks.Dashboard}>
+              <i className="bi bi-arrow-left me-1"></i>
+            </Link>
+          </span>
+          Delivery
+        </h5>
+        <hr />
+      </div>
+    </>
+  );
 }
 
-export default Delivery
+export default Delivery;
