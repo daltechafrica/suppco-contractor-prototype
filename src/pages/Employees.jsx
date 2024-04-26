782cc763c26ff2bc3f73783443ecafc3db46dca6
dalienst <oduordalienst@gmail.com>
2024-04-26 11:38:55 +0300

    add more employees

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,20 +1,180 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { appLinks } from "../constants/links";
 import { employees } from "../data/employees";
+import Modal from "react-bootstrap/Modal";
 
 function Employees() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = () => {
+    setShow(false);
+    alert("Employee Added and allocated to site");
+  };
   return (
     <>
       <div className="container py-3 mb-3">
-        <h5>
-          <span>
-            <Link to={appLinks?.Dashboard}>
-              <i className="bi bi-arrow-left me-1"></i>
+        <section className="d-flex justify-content-between align-items-center">
+          <h5>
+            <span>
+              <Link to={appLinks?.Dashboard}>
+                <i className="bi bi-arrow-left me-1"></i>
+              </Link>
+            </span>
+            Employees
+          </h5>
+          <div className="d-flex justify-content-around align-items-center">
+            <Link
+              onClick={() =>
+                alert(
+                  "Registration Link copied. Send it to the potential employee"
+                )
+              }
+              className="btn btn-sm btn-outline-primary me-1"
+            >
+              Copy
             </Link>
-          </span>
-          Orders
-        </h5>
+            <Link onClick={handleShow} className="btn btn-sm btn-primary">
+              Add
+            </Link>
+          </div>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            dialogClassName="modal-dialog modal-lg"
+          >
+            <div className="modal-header">
+              <h5 className="modal-title">Add New Employee</h5>
+              <button
+                className="btn-close"
+                type="button"
+                onClick={handleClose}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="name"
+                    placeholder="Enter employee name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="username">
+                    Username
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="username"
+                    placeholder="Enter employee username"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    id="email"
+                    placeholder="Enter employee email"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="phone">
+                    Phone
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="phone"
+                    placeholder="Enter employee phone"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="address">
+                    Address
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="address"
+                    placeholder="Enter employee address"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="role">
+                    Role
+                  </label>
+                  <select name="role" id="" className="form-select">
+                    <option value="Foreman">Foreman</option>
+                    <option value="Driver">Driver</option>
+                    <option value="Accountant">Accountant</option>
+                    <option value="Admin">Admin</option>
+                    <option value="Site Manager">Site Manager</option>
+                    <option value="Other">Other</option>
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="site">
+                    Site
+                  </label>
+                  <select name="site" id="" className="form-select">
+                    <option value="Site A">Site A</option>
+                    <option value="Site B">Site B</option>
+                    <option value="Site C">Site C</option>
+                    <option value="Site D">Site D</option>
+                  </select>
+                </div>
+                <h6>For Drivers only</h6>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="license">
+                    License
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="license"
+                    placeholder="Enter employee license"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="licenseExpiry">
+                    License Expiry
+                  </label>
+                  <input
+                    className="form-control"
+                    type="date"
+                    id="licenseExpiry"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="plateReg">
+                    Plate Registration
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="plateReg"
+                    placeholder="Enter employee plate registration"
+                  />
+                </div>
+                <div className="mb-3">
+                  <button className="btn btn-outline-success" type="submit" onClick={handleSubmit}>Save</button>
+                </div>
+              </form>
+            </div>
+          </Modal>
+        </section>
         <hr />
         <section className="mb-3">
           <div className="table-responsive">
@@ -27,19 +187,21 @@ function Employees() {
                   <th>Phone</th>
                   <th>Address</th>
                   <th>Role</th>
+                  <th>Site</th>
                 </tr>
               </thead>
               <tbody>
                 {employees?.map((employee) => (
                   <>
-                  <tr key={employee.id}>
-                    <td>{employee.name}</td>
-                    <td>{employee.username}</td>
-                    <td>{employee.email}</td>
-                    <td>{employee.phone}</td>
-                    <td>{employee.address}</td>
-                    <td>{employee.role}</td>
-                  </tr>
+                    <tr key={employee.id}>
+                      <td>{employee.name}</td>
+                      <td>{employee.username}</td>
+                      <td>{employee.email}</td>
+                      <td>{employee.phone}</td>
+                      <td>{employee.address}</td>
+                      <td>{employee.role}</td>
+                      <td>{employee.site}</td>
+                    </tr>
                   </>
                 ))}
               </tbody>
