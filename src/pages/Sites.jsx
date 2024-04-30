ba309b02c87ef4683c24d527c7abd2e20a0e9c9e
dalienst <oduordalienst@gmail.com>
2024-04-30 18:05:44 +0300

    complete sites

--- src/pages/Sites.jsx
+++ src/pages/Sites.jsx
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { appLinks } from "../constants/links";
 import { sites } from "../data/sites";
+import Modal from "react-bootstrap/Modal";
 
 function Sites() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="container mb-3 py-3">
-        <h5>
-          <span>
-            <Link to={appLinks?.Dashboard}>
-              <i className="bi bi-arrow-left me-1"></i>
-            </Link>
-          </span>
-          Sites
-        </h5>
+        <section className="d-flex justify-content-between align-items-center">
+          <h5>
+            <span>
+              <Link to={appLinks?.Dashboard}>
+                <i className="bi bi-arrow-left me-1"></i>
+              </Link>
+            </span>
+            Sites
+          </h5>
+
+          <Link onClick={handleShow} className="btn btn-outline-success btn-sm">
+            Add Site
+          </Link>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            dialogClassName="modal-dialog"
+          >
+            <div className="modal-header">
+              <h5 className="modal-title">Add Site</h5>
+              <button
+                className="btn-close"
+                type="button"
+                onClick={handleClose}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="name">
+                    Name
+                  </label>
+                  <input className="form-control" type="text" id="name" />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="address">
+                    Address
+                  </label>
+                  <input className="form-control" type="text" id="address" />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="phone">
+                    Phone
+                  </label>
+                  <input className="form-control" type="text" id="phone" />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="image">
+                    Image
+                  </label>
+                  <input className="form-control" type="text" id="image" />
+                </div>
+
+                <div className="mb-3">
+                  <Link
+                    onClick={handleClose}
+                    className="btn btn-sm btn-outline-success"
+                  >
+                    Add
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </Modal>
+        </section>
         <hr />
         <section className="mb-3">
           <div className="row">
