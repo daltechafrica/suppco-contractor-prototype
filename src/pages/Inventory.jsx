aacb5d393444ebe9103ace99d69fc07da95f4001
dalienst <oduordalienst@gmail.com>
2024-05-01 15:22:57 +0300

    add inventory data

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { appLinks } from "../constants/links";
+import { shell } from "../data/shellEquipment";
+import { buildersPlant } from "../data/buildersPlant";
 
 function Inventory() {
+  const ShellEquipmentCount = shell?.length;
+  const BuildersCount = buildersPlant?.length;
+
   return (
     <>
       <div className="container mb-3 py-3">
@@ -16,8 +21,42 @@ function Inventory() {
         </h5>
 
         <section>
-          <div className="row">
-            <div className="col-md-6 col-sm-12"></div>
+          <div className="table-responsive">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Category</th>
+                  <th>Count</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Builders Plant</td>
+                  <td>{BuildersCount}</td>
+                  <td>
+                    <Link
+                      to={appLinks?.Builders}
+                      className="btn btn-sm btn-outline-primary"
+                    >
+                      View
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Shell Equipment</td>
+                  <td>{ShellEquipmentCount}</td>
+                  <td>
+                    <Link
+                      to={appLinks?.ShellEquipment}
+                      className="btn btn-sm btn-outline-primary"
+                    >
+                      View
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </section>
       </div>
